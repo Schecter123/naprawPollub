d978cb8da4b38b946c2c117f45826eb3898167fb
Paweł <kosik.pawel@outlook.com>
2019-10-09 17:08:27 +0200

    Pytania i odpowiedzi 1

--- client/src/app/components/questions/questions.component.ts
+++ client/src/app/components/questions/questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-questions',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
+    if(!this.authService.isLogged)
+    {
+      this.authService.show();
+    }
   }
 
 }

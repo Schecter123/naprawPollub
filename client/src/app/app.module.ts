2a34ce8cde314be65f8b2b630fa58a1b2c512978
Paweł <kosik.pawel@outlook.com>
2019-10-21 18:39:34 +0200

    rejestracja i toastr

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { RoomService } from './shared/services/room.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MarkerService } from './shared/services/marker.service';
+import { MessageService } from 'src/app/shared/services/message.service';
+import {ToastrModule} from 'ngx-toastr';
 
 //import { AgmCoreModule } from '@agm/core';
 
@@ -26,6 +28,7 @@ import { MarkerService } from './shared/services/marker.service';
     BrowserAnimationsModule,
     MaterialModule,
     ComponentModule,
+    ToastrModule.forRoot()
     // AgmCoreModule.forRoot({
     //   apiKey: 'AIzaSyD7V_B_3KdjW6_cZrU3x3DxOS1SU_BMD20'
     // })
@@ -35,7 +38,8 @@ import { MarkerService } from './shared/services/marker.service';
     PlaceService,
     DefectService,
     RoomService,
-    MarkerService
+    MarkerService,
+    MessageService
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserType } from 'src/app/shared/models/user.model';
+import { MessageService } from 'src/app/shared/services/message.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { PlaceService } from 'src/app/shared/services/place.service';
+import { RegisterService } from 'src/app/shared/services/register.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +16,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  places;
+  selectedPlaces;
+  selectedPlacesId = [];
+  login: string;
+  name: string;
+  surname: string;
+  password: string;
+  email: string;
+  users;
+  user;
+  UserType = UserType;
+  subscriptionPlace: Subscription;
+
+
+  constructor(private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router,
+    private placeService: PlaceService,
+    private registerService: RegisterService) { }
 
   ngOnInit() {
+    this.authService.showRegister = false;
+
+    this.subscriptionPlace = this.placeService.getPlaces().subscribe(response => {
+      this.places = response;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscriptionPlace.unsubscribe();
+  }
+
+  // changeNameToId(items) {
+  //   for (let item of items) {
+  //     for (let place of this.places) {
+  //       if (item == place.name) {
+  //         this.selectedPlacesId.push(place.id);
+  //       }
+  //     }
+  //   }
+  // }
+
+  // checkIfUserExist(login) {
+  //   if (this.authService.loginExist(login)) {
+  //     this.messageService.error('Taki użytkownik już istnieje');
+  //     return;
+  //   }
+  // }
+
+  // addFollows(user) {
+  //   for (let i = 0; i < this.selectedPlacesId.length; i++) {
+  //     this.follow = {
+  //       id: this.follows.length + i,
+  //       idPlace: this.selectedPlacesId[i],
+  //       idUser: user.id
+  //     }
+  //     this.follows.push(this.follow);
+  //   }
+  // }
+
+  addUser() {
+    this.registerService.registerUser({
+      id: null,
+      type: UserType.Standard,
+      login: this.login,
+      password: this.password,
+      email: this.email,
+      name: this.name,
+      surname: this.surname
+    }).subscribe(
+      () => {
+        this.messageService.success('Zostałeś zarejestrowany!');
+        this.router.navigate(['/usterki/']);
+      },
+      err => { this.messageService.error('Błąd rejestracji!'); }
+    );
   }
 
 }

bbe3b4bd3340b2c35815c7cef7764326d291592f
Kuba <kuba.radomski467@gmail.com>
2019-11-24 11:05:04 +0100

    Naprawa ścieżki, skrócenie czasu trwania osobistego tokena

--- server/app/Providers/AuthServiceProvider.php
+++ server/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
 
@@ -26,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addDays(7));
+
 
         //
     }
--- server/routes/api.php
+++ server/routes/api.php
@@ -37,7 +37,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'API'], function () {
     Route::get('/defects/{defect}/room', 'DefectController@getParticularRoom');
     Route::get('/defects/{defect}/place', 'DefectController@getParticularPlace');
     Route::get('/defects/{defect}/login', 'DefectController@getDefectByLogin');
-    Route::get('/defects/{defect}/admin', 'DefectController@getDefectForAdministrator');
+    Route::get('/defects/{defect}/admin', 'DefectController@getDefectForBuildingAdministrator');
 
     Route::apiResource('comments', 'CommentController');
     Route::get('/comments/{comment}/user', 'CommentController@getCommentByIdUser');
